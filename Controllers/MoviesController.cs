8819c9504407b9c7bb8cd8a6f6fa430b2445f51c
MLim619 <MLim619@gmail.com>
2017-02-27 23:29:32 -0800

    Add files via upload

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using System.Linq;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ViewResult Index()
+        {
+            var movies = _context.Movie.Include(m => m.Genre).ToList(); //GetMovies();
+
+            return View(movies);    
+        }
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movie.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+    }
+}
